154f937b0998b12553a7805c0d59f846669bc239
Evandro Santos <vandersant7@gmail.com>
2025-11-01 17:18:53 -0300

    feat: base inicial do projeto com configuração do TarefaContext

---
+++ Data/TarefaContext.cs
@@ -0,0 +1,23 @@
+﻿using System.Reflection.Emit;
+using Microsoft.EntityFrameworkCore;
+using OrganizadorTarefa.Models;
+
+namespace OrganizadorTarefa.Data
+{
+    public class TarefaContext : DbContext
+    {
+        public TarefaContext(DbContextOptions<TarefaContext> options) : base(options)
+        {
+        }
+        public DbSet<Tarefa> Tarefas => Set<Tarefa>();
+
+        protected override void OnModelCreating(ModelBuilder mb)
+        {
+            mb.Entity<Tarefa>().HasData(
+            new Tarefa { Id = 1, Titulo = "Comprar leite", Descricao = "Supermercado", Status = EnumStatusTarefa.Pendente },
+            new Tarefa { Id = 2, Titulo = "Estudar .NET", Descricao = "Minimal APIs", Status = EnumStatusTarefa.EmAndamento },
+            new Tarefa { Id = 3, Titulo = "Correr", Descricao = "Parque", Status = EnumStatusTarefa.Concluida }
+        );
+        }
+    }
+}
